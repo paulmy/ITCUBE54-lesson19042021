24803638ed3fb8a92e07ad9abf68845b96b0a693
paulmy <paulmul85@gmail.com>
2021-04-19 17:31:52 +0700

    Update

--- app/src/main/java/com/example/lesson12042021/MyDraw.java
+++ app/src/main/java/com/example/lesson12042021/MyDraw.java
@@ -9,24 +9,100 @@ import android.view.View;
 public class MyDraw extends View {
     public MyDraw(Context context) {
         super(context);
+        Init();
     }
 
+    int N = 50;
     Paint paint = new Paint();
+    int[] R = new int[N];
+    int[] G = new int[N];
+    int[] B = new int[N];
+    int[] x = new int[N];
+    int[] y = new int[N];
+    int[] dx = new int[N];
+    int[] dy = new int[N];
+
+    void fillRandom(int[] array) {
+        for (int i = 0; i < array.length; i++) {
+            array[i] = 100 + (int) (Math.random() * 500);
+        }
+    }
+
+    void fillRandomColor(int[] array) {
+        for (int i = 0; i < array.length; i++) {
+            array[i] = (int) (Math.random() * 256);
+        }
+    }
+
+    void fillRandomdxy(int[] array) {
+        for (int i = 0; i < array.length; i++) {
+            array[i] = (int) (Math.random() * 7 - 3);
+        }
+    }
+
+    void Init() {
+        fillRandom(x);
+        fillRandom(y);
+        fillRandom(R);
+        fillRandomdxy(dx);
+        fillRandomdxy(dy);
+        fillRandomColor(R);
+        fillRandomColor(G);
+        fillRandomColor(B);
+    }
 
     @Override
     protected void onDraw(Canvas canvas) {
         super.onDraw(canvas);
-        paint.setColor(Color.BLUE);
-        paint.setStrokeWidth(5f);
-        paint.setStyle(Paint.Style.STROKE);
-        int y = 0;
-        int x = 0;
-        int R = canvas.getWidth() / 2;
+
+
+        for (int i = 0; i < N; i++) {
+            paint.setColor(Color.rgb(R[i], G[i], B[i]));
+            //paint.setStrokeWidth(5f);
+            paint.setStyle(Paint.Style.FILL);
+            canvas.drawCircle(x[i], y[i], 50, paint);
+
+        }
+        for (int i = 0; i < N; i++) {
+            if (x[i] + 50 > canvas.getWidth() || x[i] - 50 < 0)
+                dx[i] = -dx[i];
+
+            if (y[i] + 50 > canvas.getHeight() || y[i] - 50 < 0)
+                dy[i] = -dy[i];
+
+            x[i] += dx[i];
+            y[i] += dy[i];
+        }
+
+/*
+        int count = 1;
+        for (int y = 0; y < canvas.getHeight(); y = y + 100) {//Количество строк
+            count=0;
+            for (int x = 0; x < canvas.getWidth(); x = x + 100) {//Количество столбцов
+                if (count % 2 == 0) {
+                    paint.setColor(Color.BLACK);
+                    paint.setStrokeWidth(5f);
+                    paint.setStyle(Paint.Style.FILL);
+                    count=1;
+                } else {
+                    paint.setColor(Color.WHITE);
+                    paint.setStrokeWidth(5f);
+                    paint.setStyle(Paint.Style.FILL);
+                    count=0;
+                }
+
+                canvas.drawRect(x, y, x + 100, y + 100, paint);
+
+            }
+
+        }
+*/
+        /*int R = canvas.getWidth() / 2;
         while (R > 0) {
             paint.setColor(Color.rgb((int)(Math.random()*255),(int)(Math.random()*255),(int)(Math.random()*255)));
             canvas.drawCircle(canvas.getWidth() / 2,
                     canvas.getHeight() / 2, R, paint);
-            R -= 100;
+            R = R/2;
         }
         paint.setColor(Color.BLACK);
         canvas.drawLine(0,
@@ -38,6 +114,8 @@ public class MyDraw extends View {
                 canvas.getWidth() / 2,
                 canvas.getHeight(), paint);
         canvas.drawRect(20, 20, 100, 200, paint);
+*/
+
 
         /*while (y<canvas.getHeight()) {
             canvas.drawLine(0, y, canvas.getWidth(), y, paint);
@@ -49,5 +127,6 @@ public class MyDraw extends View {
             x+=100;
 
         }*/
+        invalidate();
     }
 }
